055e3e50cd7ecd3ec49d3a683fc8a492e8da79ac
fabiocardosojr <fabio.cardosojr@gmail.com>
2017-07-31 12:34:16 -0300

    classe connect

--- FindFer-Project/model/Connection.php
+++ FindFer-Project/model/Connection.php
@@ -1,7 +1,31 @@
 <?php
-class Connection{
+
+class Connection
+{
+    private $user;
+    private $password;
+    private $db;
+    private $server;
+    private static $pdo;
     
-    function __construct() {
-        
+    public function __construct(){
+        $this->servido = "localhost";
+        $this->banco = "findfer";
+        $this->usuario = "root";
+        $this->senha = "";
+    }
+    
+    public function connect(){
+        try {
+            if (is_null(self::$pdo)) {
+                self::$pdo = new PDO("mysql:host=".$this->server.";dbname=".$this->db, $this->user, $this->password);
+            }
+            
+            return self::$pdo;
+            
+        } catch (\PDOException $e) {
+            echo $ex->getMessage();
+        }
     }
 }
+
