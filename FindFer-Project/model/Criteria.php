53af7791b49160ccb7ca6d7df92fa8894a2aa6dd
infsolution <clsinfsolution@gmail.com>
2017-08-01 10:36:05 -0300

    add api for data manipulation

---
+++ FindFer-Project/model/Criteria.php
@@ -0,0 +1,31 @@
+<?php
+class Criteria extends Expression{
+    private $expressions;
+    private $operators;
+    private $properties;
+    
+            
+    function add(Expression $expression, $operator = self::AND_OPERATOR){
+        if(empty($this->expressions)){
+            unset($operator);
+        }
+        $this->expressions[] = $expression;
+        $this->operators[] = $operator;
+    }
+    function dump(){
+        if(is_array($this->expressions)){
+            foreach ($this->expressions as $i => $expression) {
+                $operator = $this->operators[$i];
+                $result .= $operator. $expression->dump() . ' ';
+            }
+            $result = trim($result);
+            return "({$result})";
+        }
+    }
+    function setProperty($property, $value){
+        $this->properties[$property] = $value;
+    }
+    function getProperty($property){
+        return $this->properties[$property];
+    }
+}
